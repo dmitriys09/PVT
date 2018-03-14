b80657cfe6dbf41796f598017759f9964ced5415
Dmitriy <dima972005@gmail.com>
2018-03-15 02:15:56 +0700

    push results

--- pct-spring2018-lab1-src/loop/loop.c
+++ pct-spring2018-lab1-src/loop/loop.c
@@ -14,7 +14,8 @@ int main()
 {
     int *v, i, sum;
     double t;
-    
+    int t1, t2, t3, t4, t5, t6, t7, t8,
+    t9, t10, t11, t12, t13, t14, t15, t16;
     if ( (v = malloc(sizeof(*v) * n)) == NULL) {
         fprintf(stderr, "No enough memory\n");
         exit(EXIT_FAILURE);
@@ -25,9 +26,29 @@ int main()
    
     t = hpctimer_wtime();
     /* TODO: Unroll this loop */
-    for (sum = 0, i = 0; i < n; i++) {
-        sum += v[i];
+    t1 = t2 = t3 = t4 = t5 = t6 = t7 = t8 = 
+    t9 = t10 = t11 = t12 = t13 = t14 = t15 = t16 = 0;
+    for (sum = 0, i = 15; i < n; i += 16) {
+        //sum += v[i];
+        t1 += v[i - 15];
+        t2 += v[i - 14];
+        t3 += v[i - 13];
+        t4 += v[i - 12];
+        t5 += v[i - 11];
+        t6 += v[i - 10];
+        t7 += v[i - 9];
+        t8 += v[i - 8];
+        t9 += v[i - 7];
+        t10 += v[i - 6];
+        t11 += v[i - 5];
+        t12 += v[i - 4];
+        t13 += v[i - 3];
+        t14 += v[i - 2];
+        t15 += v[i - 1];
+        t16 += v[i];
     }
+    sum = t1 + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9 + t10 +
+    t11 + t12 + t13 + t14 + t15 + t16;
     t = hpctimer_wtime() - t;
 
     printf("Sum = %d\n", sum);
